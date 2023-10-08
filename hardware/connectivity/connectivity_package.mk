f3a4763f6c1c9491ac5d1eb972a436f348a4bfa7
Zinadin Zidan <zidan.roking@gmail.com>
2023-10-08 09:36:42 +0000

    Test Source wifi hal

--- hardware/connectivity/connectivity_package.mk
+++ hardware/connectivity/connectivity_package.mk
@@ -5,5 +5,4 @@ PRODUCT_PACKAGES += \
     libkeystore-wifi-hidl \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf \
-    libkeystore-engine-wifi-hidl \
-    android.hardware.wifi@1.0-service-lazy-mediatek
+    libkeystore-engine-wifi-hidl
